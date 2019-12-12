ab28b0893b4a938a7895fbc5da80d3221d41cc0d
kirsa <kirsa@LAPTOP-BDNOU8FR>
2019-12-12 15:04:42 +0300

    Add 4 lab.

---
+++ Laba4/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace Laba4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxSearch = new System.Windows.Forms.TextBox();
+            this.labelReadTime = new System.Windows.Forms.Label();
+            this.labelCount = new System.Windows.Forms.Label();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.labelTimeSearch = new System.Windows.Forms.Label();
+            this.listBoxResult = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(40, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(236, 58);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "считать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxSearch
+            // 
+            this.textBoxSearch.Location = new System.Drawing.Point(442, 48);
+            this.textBoxSearch.Name = "textBoxSearch";
+            this.textBoxSearch.Size = new System.Drawing.Size(168, 26);
+            this.textBoxSearch.TabIndex = 3;
+            // 
+            // labelReadTime
+            // 
+            this.labelReadTime.AutoSize = true;
+            this.labelReadTime.Location = new System.Drawing.Point(132, 151);
+            this.labelReadTime.Name = "labelReadTime";
+            this.labelReadTime.Size = new System.Drawing.Size(18, 20);
+            this.labelReadTime.TabIndex = 4;
+            this.labelReadTime.Text = "0";
+            // 
+            // labelCount
+            // 
+            this.labelCount.AutoSize = true;
+            this.labelCount.Location = new System.Drawing.Point(132, 118);
+            this.labelCount.Name = "labelCount";
+            this.labelCount.Size = new System.Drawing.Size(18, 20);
+            this.labelCount.TabIndex = 5;
+            this.labelCount.Text = "0";
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Location = new System.Drawing.Point(629, 48);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(136, 42);
+            this.buttonSearch.TabIndex = 6;
+            this.buttonSearch.Text = "Найти";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
+            // 
+            // labelTimeSearch
+            // 
+            this.labelTimeSearch.AutoSize = true;
+            this.labelTimeSearch.Location = new System.Drawing.Point(478, 118);
+            this.labelTimeSearch.Name = "labelTimeSearch";
+            this.labelTimeSearch.Size = new System.Drawing.Size(18, 20);
+            this.labelTimeSearch.TabIndex = 7;
+            this.labelTimeSearch.Text = "0";
+            // 
+            // listBoxResult
+            // 
+            this.listBoxResult.FormattingEnabled = true;
+            this.listBoxResult.ItemHeight = 20;
+            this.listBoxResult.Location = new System.Drawing.Point(442, 161);
+            this.listBoxResult.Name = "listBoxResult";
+            this.listBoxResult.Size = new System.Drawing.Size(266, 184);
+            this.listBoxResult.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBoxResult);
+            this.Controls.Add(this.labelTimeSearch);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.labelCount);
+            this.Controls.Add(this.labelReadTime);
+            this.Controls.Add(this.textBoxSearch);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxSearch;
+        private System.Windows.Forms.Label labelReadTime;
+        private System.Windows.Forms.Label labelCount;
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.Label labelTimeSearch;
+        private System.Windows.Forms.ListBox listBoxResult;
+    }
+}
+
---
+++ Laba4/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laba4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<string> lst = new List<string>();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fldlg = new OpenFileDialog();
+            fldlg.Filter = "Текстовый файл |*.txt";
+            if (fldlg.ShowDialog() == DialogResult.OK)
+            {
+                Stopwatch time = new Stopwatch();
+                time.Start();
+                string text = File.ReadAllText(fldlg.FileName);
+                char[] separ = { '\n', ' ', '.', ',', '\t', '/', '?' };
+                string[] words = text.Split(separ);
+                foreach (string word in words)
+                {
+                    string wrd = word.Trim();
+                    if (!lst.Contains(wrd)) lst.Add(wrd);
+                }
+                time.Stop();
+                this.labelReadTime.Text = time.Elapsed.ToString();
+                this.labelCount.Text = lst.Count().ToString();
+            }
+            else
+            {
+                MessageBox.Show("Необходимо выбрать файл.");
+            }
+
+        }
+
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+            string word = textBoxSearch.Text.Trim();
+            if (!string.IsNullOrWhiteSpace(word) && lst.Count > 0)
+            {
+                word = word.ToUpper();
+                List<string> res = new List<string>();
+                Stopwatch t = new Stopwatch();
+                t.Start();
+                foreach (string wrd in lst)
+                {
+                    if (wrd.ToUpper().Contains(word))
+                    {
+                        res.Add(wrd);
+                    }
+                }
+                t.Stop();
+                this.labelTimeSearch.Text = t.Elapsed.ToString();
+                this.listBoxResult.BeginUpdate();
+                this.listBoxResult.Items.Clear();
+
+                foreach (string wrd in res)
+                {
+                    this.listBoxResult.Items.Add(wrd);
+                }
+                this.listBoxResult.EndUpdate();
+
+            }
+            else
+            {
+                MessageBox.Show("Необходимо выбрать файл и ввести слово для поиска");
+            }
+
+        }
+    }
+}
